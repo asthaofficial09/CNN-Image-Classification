b5b9fc0803d1f8ee9581fa79151850eb44cefd0c
Astha Mishra <asthaofficial09@gmail.com>
2025-10-25 15:28:32 +0400

    Load model without compilation during evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -8,7 +8,7 @@ import numpy as np
 
 (_, _), (x_test, y_test) = datasets.cifar10.load_data()
 x_test = x_test / 255.0
-model = load_model("models/cnn_cifar10_model.h5")
+model = load_model("models/cnn_cifar10_model.h5", compile= False)
 
 loss, acc = model.evaluate(x_test, y_test)
 print(f"Test Accuracy: {acc:.3f}")
